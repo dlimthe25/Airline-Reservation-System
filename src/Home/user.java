5c551a6d1734bb6f25d6b181f6b2d5b63e553ae5
dlimthe25 <69019450+dlimthe25@users.noreply.github.com>
2021-06-14 00:22:40 +0800

    Updates

---
+++ src/Home/user.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Home;
+
+/**
+ *
+ * @author David
+ */
+public class user {
+    
+    public static int userid;
+    public static String userfullname;
+    public static String permission;
+    
+}
